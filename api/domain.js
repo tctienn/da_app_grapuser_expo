38e0358704f5a34c12345444b8e2d5eee50c53c7
tctienn <mainichichimvm@gmail.com>
2024-12-28 22:59:34 +0700

    vẫn đagng lõi qrqr

--- api/domain.js
+++ api/domain.js
@@ -1,6 +1,15 @@
-// export const url = "http://" + ip + ":8081"
-export const ip = "171.244.60.77"
-// const ip = "172.20.10.2"  // iphone
-export const urlProduct = "http://" + ip + ":8080"
-export const urlGrap = "http://" + ip + ":8081"
-export const urlNotification = "http://" + ip + ":8083"
+// // export const url = "http://" + ip + ":8081"
+// export const ip = "171.244.60.77"
+
+
+// // const ip = "172.20.10.2"  // iphone
+// export const urlProduct = "http://" + ip + ":8080"
+// export const urlGrap = "http://" + ip + ":8081"
+// export const urlNotification = "http://" + ip + ":8083"
+
+
+
+export const ip = "tcds.io.vn"
+export const urlProduct = "http://" + ip
+export const urlGrap = "http://" + ip
+export const urlNotification = "http://" + ip 
